413ce92546c8f106a9dcfc91cf7f869a804b5a93
Richard <richard@gsdh.org>
2014-11-14 12:11:35 +0200

    Allow for dynamic number of columns
    Added option for adding an attribute to the body tag for customizing
    number of columns that are overlayed.
    Attribute is optional, fallback to 12 columns.
    
    <body data-bootstrap-num-cols="15">

--- grid.js
+++ grid.js
@@ -1,26 +1,30 @@
 if (document.getElementsByClassName('cb-grid-lines').length){
-    
+
     document.body.removeChild(document.getElementsByClassName('cb-grid-lines')[0]);
 }
 
 else {
-    document.body.innerHTML += '<div class="cb-grid-lines"> \
+
+    var html = "",
+        numCols = 12,
+        dataNumCols = document.getElementsByTagName("body")[0].getAttribute("data-bootstrap-num-cols");
+
+    if(typeof dataNumCols === "string" && dataNumCols.length){
+        numCols = parseInt(dataNumCols, 10);
+    }
+
+    html += '<div class="cb-grid-lines"> \
       <div class="container"> \
-        <div class="row"> \
-            <div class="span1 col-xs-1"></div> \
-            <div class="span1 col-xs-1"></div> \
-            <div class="span1 col-xs-1"></div> \
-            <div class="span1 col-xs-1"></div> \
-            <div class="span1 col-xs-1"></div> \
-            <div class="span1 col-xs-1"></div> \
-            <div class="span1 col-xs-1"></div> \
-            <div class="span1 col-xs-1"></div> \
-            <div class="span1 col-xs-1"></div> \
-            <div class="span1 col-xs-1"></div> \
-            <div class="span1 col-xs-1"></div> \
-            <div class="span1 col-xs-1"></div> \
-        </div> \
+        <div class="row">';
+
+    for (var i = 0; i < numCols; i += 1) {
+        html += '<div class="span1 col-xs-1"></div>';
+    }
+
+    html += '</div> \
       </div> \
     </div>';
-    
+
+    document.body.innerHTML += html;
+
 }
